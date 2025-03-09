ce25b1c7ef4f59ba0a82d913d280077d11758504
Aqzhol <145491865+aqzhoool@users.noreply.github.com>
2025-03-09 21:23:36 +0000

    trying to publish

--- waves.js
+++ waves.js
@@ -16,7 +16,7 @@ class WaveBackground {
     }
   
     init() {
-      // creating wave
+      // wave create
       for(let i = 0; i < 3; i++) {
         this.waves.push({
           amplitude: 50 + Math.random() * 100,
